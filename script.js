9ce862773d5658becc79d1fdfe5e6b4b0b43bc54
Faisal Ali <fali77616@gmail.com>
2025-01-11 09:32:06 +0530

    payment added successfully

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ console.log(button);
 let login = "Login page added";
 console.log(login);
 
+<<<<<<< HEAD
 let footer = "added footer in our website";
 console.log(footer);
+=======
+
+let payment = "intrigated payment gateway";
+console.log(payment);
+>>>>>>> 6266548 (payment added)
