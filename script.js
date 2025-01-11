71752dac7c7df36031df9fd738f3f62d7a19a045
Faisal Ali <fali77616@gmail.com>
2025-01-11 09:33:37 +0530

    UPI added before footer

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ console.log(button);
 let login = "Login page added";
 console.log(login);
 
-<<<<<<< HEAD
-let footer = "added footer in our website";
-console.log(footer);
-=======
-
 let payment = "intrigated payment gateway";
 console.log(payment);
->>>>>>> 6266548 (payment added)
+
+let upi = "intrigated UPI also";
+console.log(upi);
+
+
+let footer = "added footer in our website";
+console.log(footer);
